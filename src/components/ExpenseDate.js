64b15d7c4c278ab72d849dea0c7adffd6a3ac4ee
user <kamauri84@gmail.com>
2021-04-19 23:02:29 +1000

    add styling to expense data component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -6,12 +6,10 @@ const ExpenseDate = (props) => {
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
-    <div>
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   );
 };
